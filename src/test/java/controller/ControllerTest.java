81ea5bf4f7839e8d5aa4dbd3b56f42291fa7c1c7
Alexandru <stoica.alexandru2000@gmail.com>
2017-03-09 05:51:33 -0800

    Initial Commit

---
+++ src/test/java/controller/ControllerTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/model/ModelTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
