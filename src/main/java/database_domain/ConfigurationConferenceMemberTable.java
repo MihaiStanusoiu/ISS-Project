5833e43c4c050f854bd1bac52ba0956695ad891b
Tiron Andreea <tiron.andreea.@gmail.com>
2017-04-08 17:59:53 +0300

    Database domain for section, section member and configuration section member.

--- src/main/java/database_domain/ConfigurationConferenceMemberTable.java
+++ src/main/java/database_domain/ConfigurationConferenceMemberTable.java
@@ -10,6 +10,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 /**
  * Name:         ConfigurationConferenceMemberTable
+ * Effect:       Class for the db table ConfigurationConferenceMember.
  * Date:         08/04/2017
  * Tested:       False
  * @author       Tiron Andreea- Ecaterina
@@ -37,34 +38,66 @@ public class ConfigurationConferenceMemberTable {
     public ConfigurationConferenceMemberTable(){
     }
 
+    /**
+     * Effect: Returns the id of the conference member.
+     * @return [Integer] : returns the id.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Effect: Sets the id of a configuration conference member.
+     * @param id: new value for id.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Effect: Return whether the user is a chair.
+     * @return [Boolean] : returns true if it is a chair, false otherwise.
+     */
     public Boolean getChair() {
         return isChair;
     }
 
+    /**
+     * Effect: Sets whether the user is a chair.
+     * @param chair: new value for chair.
+     */
     public void setChair(Boolean chair) {
         isChair = chair;
     }
 
+    /**
+     * Effect: Return whether the user is a co-chair.
+     * @return [Boolean] : returns true if it is a co-chair, false otherwise.
+     */
     public Boolean getCochair() {
         return isCochair;
     }
 
+    /**
+     * Effect: Sets whether the user is a co-chair.
+     * @param cochair: new value for co-chair.
+     */
     public void setCochair(Boolean cochair) {
         isCochair = cochair;
     }
 
+    /**
+     * Effect: Return whether the user is a pc-member.
+     * @return [Boolean] : returns true if it is a pc-member, false otherwise.
+     */
     public Boolean getPcmember() {
         return isPcmember;
     }
 
+    /**
+     * Effect: Sets whether the user is a pc-member.
+     * @param pcmember: new value for pc-member.
+     */
     public void setPcmember(Boolean pcmember) {
         isPcmember = pcmember;
     }
--- src/main/java/database_domain/ConfigurationSectionMemberTable.java
+++ src/main/java/database_domain/ConfigurationSectionMemberTable.java
@@ -10,10 +10,11 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 /**
  * Name:         ConfigurationSectionMemberTable
+ * Effect:       Class for the db table ConfigurationSectionMember
  * Date:         08/04/2017
  * Tested:       False
- * @author Tiron Andreea- Ecaterina
- * @version 1.0
+ * @author       Tiron Andreea- Ecaterina
+ * @version      1.0
  */
 
 @Entity
@@ -35,10 +36,18 @@ public class ConfigurationSectionMemberTable {
     @OneToMany(mappedBy = "idConfiguration")
     private ArrayList<SectionMember> sectionMembers = new ArrayList<>();
 
+    /**
+     * Effect: Return the section members with this configuration.
+     * @return [ArrayList<SectionMember>] : returns the list of members.
+     */
     public ArrayList<SectionMember> getSectionMembers() {
         return this.sectionMembers;
     }
 
+    /**
+     * Effect: Sets the list of members to a section configuration.
+     * @param sectionMembers : new value for section members.
+     */
     public void setSectionMembers(ArrayList<SectionMember> sectionMembers) {
         this.sectionMembers = sectionMembers;
     }
@@ -46,34 +55,66 @@ public class ConfigurationSectionMemberTable {
     public ConfigurationSectionMemberTable(){
     }
 
+    /**
+     * Effect: Return the id of a configuration section member.
+     * @return [Integer] : returns the id.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Effect: Sets the id of a configuration section member.
+     * @param id : new value for id.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Effect: Return whether the user is a chair.
+     * @return [Boolean] : returns true if it is a chair, false otherwise.
+     */
     public Boolean getChair() {
         return isChair;
     }
 
+    /**
+     * Effect: Sets whether the user is a chair.
+     * @param chair: new value for chair.
+     */
     public void setChair(Boolean chair) {
         isChair = chair;
     }
 
+    /**
+     * Effect: Return whether the user is a speaker.
+     * @return [Boolean] : returns true if it is a speaker, false otherwise.
+     */
     public Boolean getSpeaker() {
         return isSpeaker;
     }
 
+    /**
+     * Effect: Sets whether the user is a speaker.
+     * @param speaker: new value for speaker.
+     */
     public void setSpeaker(Boolean speaker) {
         isSpeaker = speaker;
     }
 
+    /**
+     * Effect: Return whether the user is a listener.
+     * @return [Boolean] : returns true if it is a listener, false otherwise.
+     */
     public Boolean getListener() {
         return isListener;
     }
 
+    /**
+     * Effect: Sets whether the user is a listener.
+     * @param listener: new value for listener.
+     */
     public void setListener(Boolean listener) {
         isListener = listener;
     }
