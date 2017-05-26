8e9fa3f6db735437f292db602598df8798cdc13f
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2017-05-26 14:49:28 +0300

    CoreContext Starting Point

---
+++ org.confery.client/src/main/java/corecontext/UserType.java
@@ -0,0 +1,29 @@
+package corecontext;
+
+/**
+ * @author Alexandru Stoica
+ * @version 1.0
+ */
+
+public enum UserType {
+
+    /**
+     * For regular users - with no permissions over the data in relationship
+     */
+    REGULAR,
+
+    EDITION_PC_MEMBER,
+
+    EDITION_CHAIR,
+
+    EDITION_CO_CHAIR,
+
+    REVIEWER,
+
+    SECTION_CHAIR,
+
+    SECTION_SPEAKER,
+
+    SECTION_LISTENER,
+
+}
